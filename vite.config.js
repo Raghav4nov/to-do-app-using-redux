09c8631c57b0a9c02c81f4dab76b5050dd3bba9b
Raghav Parmanand <raghavparmanand@gmail.com>
2025-09-25 16:01:43 +0530

    Add homepage and vite base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/to-do-app-using-redux/', 
   plugins: [
     tailwindcss(),
   ],
